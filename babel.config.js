fb026ab707085f7537bcfbc6e39f2f23909e5200
Ben Huynh <BenH@soundcapital.com>
2025-05-11 18:34:51 -0700

    deleted old structure

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-/*converts modern JS into code compatible with older JS engines */
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-    };
-  };
